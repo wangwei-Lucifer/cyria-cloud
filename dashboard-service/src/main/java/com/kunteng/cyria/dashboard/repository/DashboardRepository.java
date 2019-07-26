e067e0e375e92f749bfd57ded8da07397db1cdc5
caizhibang <caizhibang@kunteng.org>
2019-07-26 16:53:54 +0800

    caizhibang:20190726 modify

--- dashboard-service/src/main/java/com/kunteng/cyria/dashboard/repository/DashboardRepository.java
+++ dashboard-service/src/main/java/com/kunteng/cyria/dashboard/repository/DashboardRepository.java
@@ -11,28 +11,28 @@ import org.springframework.data.domain.Pageable;
 
 @Repository
 public interface DashboardRepository extends MongoRepository<Dashboard, String> {
-	@Query(value= "{'user':?0}",fields="{'config.title':1, 'user':1, 'hash':1, 'publish.status':1, 'publish.hash':1, 'config.timestamp':1, 'publish.timestamp':1}")
+	@Query(value= "{'user':?0}",fields="{'config.title':1, 'user':1, 'hash':1, 'publish.status':1, 'publish.hash':1, 'config.timestamp':1, 'publish.timestamp':1, 'imgUrl':1}")
 	List<Dashboard> findByUser(String user, Pageable pageable);
 	
-	@Query(value= "{'publish.status':?1}",fields="{'config.title':1, 'user':1, 'hash':1, 'publish.status':1, 'publish.hash':1, 'config.timestamp':1, 'publish.timestamp':1}")
+	@Query(value= "{'publish.status':?1}",fields="{'config.title':1, 'user':1, 'hash':1, 'publish.status':1, 'publish.hash':1, 'config.timestamp':1, 'publish.timestamp':1, 'imgUrl':1}")
 	List<Dashboard> findByUserAndStatus(String user, String status,Pageable pageable);
 	
-	@Query(value= "{'config.title':?1}",fields="{'config.title':1, 'user':1, 'hash':1, 'publish.status':1,'publish.hash':1, 'config.timestamp':1, 'publish.timestamp':1}")
+	@Query(value= "{'config.title':?1}",fields="{'config.title':1, 'user':1, 'hash':1, 'publish.status':1,'publish.hash':1, 'config.timestamp':1, 'publish.timestamp':1, 'imgUrl':1}")
 	List<Dashboard> findByUserAndTitle(String user, String title,Pageable pageable);
 	
-	@Query(value= "{'config.title':?1, 'publish.status':?2}",fields="{'config.title':1, 'user':1,'hash':1, 'publish.status':1, 'publish.hash':1, 'config.timestamp':1, 'publish.timestamp':1}")
+	@Query(value= "{'config.title':?1, 'publish.status':?2}",fields="{'config.title':1, 'user':1,'hash':1, 'publish.status':1, 'publish.hash':1, 'config.timestamp':1, 'publish.timestamp':1, 'imgUrl':1}")
 	List<Dashboard> findByUserAndTitleAndStatus(String user, String title, String status, Pageable pageable);
 	
-	@Query(value= "{'user':?0, 'project':?1}",fields="{'config.title':1, 'user':1, 'hash':1, 'project':1 ,'publish.status':1, 'publish.hash':1, 'config.timestamp':1, 'publish.timestamp':1}")
+	@Query(value= "{'user':?0, 'project':?1}",fields="{'config.title':1, 'user':1, 'hash':1, 'project':1 ,'publish.status':1, 'publish.hash':1, 'config.timestamp':1, 'publish.timestamp':1, 'imgUrl':1}")
 	List<Dashboard> findByUserAndProject(String user, String project, Pageable pageable);
 	
-	@Query(value= "{'publish.status':?1, 'project':?2}",fields="{'config.title':1, 'user':1, 'hash':1, 'project':1 ,'publish.status':1, 'publish.hash':1, 'config.timestamp':1, 'publish.timestamp':1}")
+	@Query(value= "{'publish.status':?1, 'project':?2}",fields="{'config.title':1, 'user':1, 'hash':1, 'project':1 ,'publish.status':1, 'publish.hash':1, 'config.timestamp':1, 'publish.timestamp':1, 'imgUrl':1}")
 	List<Dashboard> findByUserAndStatusAndProject(String user, String status, String project,Pageable pageable);
 	
-	@Query(value= "{'config.title':?1, 'project':?2}",fields="{'config.title':1, 'user':1, 'hash':1, 'project':1 ,'publish.status':1,'publish.hash':1, 'config.timestamp':1, 'publish.timestamp':1}")
+	@Query(value= "{'config.title':?1, 'project':?2}",fields="{'config.title':1, 'user':1, 'hash':1, 'project':1 ,'publish.status':1,'publish.hash':1, 'config.timestamp':1, 'publish.timestamp':1, 'imgUrl':1}")
 	List<Dashboard> findByUserAndTitleAndProject(String user, String title, String project, Pageable pageable);
 	
-	@Query(value= "{'config.title':?1, 'publish.status':?2, 'project':?3}",fields="{'config.title':1, 'user':1,'hash':1,'project':1 , 'publish.status':1, 'publish.hash':1, 'config.timestamp':1, 'publish.timestamp':1}")
+	@Query(value= "{'config.title':?1, 'publish.status':?2, 'project':?3}",fields="{'config.title':1, 'user':1,'hash':1,'project':1 , 'publish.status':1, 'publish.hash':1, 'config.timestamp':1, 'publish.timestamp':1, 'imgUrl':1}")
 	List<Dashboard> findByUserAndTitleAndStatusAndProject(String user, String title, String status, String project ,Pageable pageable);
 	
 	//@Query(value= "{'user':?0}",count=true)
